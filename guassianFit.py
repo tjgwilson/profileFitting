2cec39f3d41666addf31278e391a40b0ed8704eb
Thomas Wilson <twilson@wasabi.astro>
2020-05-12 17:59:17 +0100

    fits 4 gaussians - 2 for the peak, 1 for each absortion if present

--- guassianFit.py
+++ guassianFit.py
@@ -3,13 +3,14 @@ from astropy import modeling
 import matplotlib.pyplot as plt
 
 #####Importing#####
-
+col = 1
 inc = ['20','50','80']
 freq = ['06562','12818','21655']
 filename = []
 for i in inc:
     for f in freq:
         filename.append("halpha_0"+i+"_"+f+".dat")
+
 fig, ax = plt.subplots(3,3)
 r=0
 c=0
@@ -17,7 +18,7 @@ check = 1
 for name in filename:
     data = np.loadtxt(name)
     # data[:,1] = data[:,1] + np.sqrt(abs(data[:,1])) * np.random.random(data[:,0].size) - 0.5
-    data[:,1] = data[:,1] - 1.
+    data[:,1] = data[:,1] - data[0,1]
 
 
     ####Fitting######
@@ -30,25 +31,31 @@ for name in filename:
     xmin2 = 0.0
 
     for i in range(len(data)):
-        if(data[i,1] >= max):
-            max = data[i,1]
+        if(data[i,col] >= max):
+            max = data[i,col]
             xmax = data[i,0]
-        if((data[i,1] <= min1) and (data[i,0] < 0.0)):
-            min1 = data[i,1]
+        if((data[i,col] <= min1) and (data[i,0] < 0.0)):
+            min1 = data[i,col]
             xmin1 = data[i,0]
-        if((data[i,1] <= min2) and (data[i,0] > 0.0)):
-            min2 = data[i,1]
+        if((data[i,col] <= min2) and (data[i,0] > 0.0)):
+            min2 = data[i,col]
             xmin2 = data[i,0]
-    print(max,min)
+    print(min1,xmin1)
 
 
     fitter = modeling.fitting.LevMarLSQFitter()
+    gaus =[]
+    gaus.append(modeling.models.Gaussian1D(max,xmax,10)) #- modeling.models.Gaussian1D()  # Lorentz1D() Voigt1D() Moffatt1D()
+    gaus.append(modeling.models.Gaussian1D(max,xmax,5))
 
-    g1 = modeling.models.Gaussian1D(max,xmax,10)
-    g2 = modeling.models.Gaussian1D(min1,xmin1,10)
-    g3 = modeling.models.Gaussian1D(min2,xmin2,10)
-    model = g1  + g2 + g3 #- modeling.models.Gaussian1D()  # Lorentz1D() Voigt1D() Moffatt1D()
-    fitted_model = fitter(model, data[:,0], data[:,1])
+    if(min1 != 0):
+        gaus.append(modeling.models.Gaussian1D(min1,xmin1,10))
+    if(min2 != 0):
+        gaus.append(modeling.models.Gaussian1D(min2,xmin2,10))
+    model = gaus[0] + gaus[1]
+    for i in range(2,len(gaus)):
+        model += gaus[i]
+    fitted_model = fitter(model, data[:,0], data[:,col])
     # for model in fitted_model:
     #     print(model)
 
