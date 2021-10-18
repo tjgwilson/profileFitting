91adca1363817877a5fa847377790fdd7bcf600e
tjgw201 <tjgw01@exeter.ac.uk>
2021-10-18 17:47:41 +0100

    updates to example

--- testFWHM.py
+++ testFWHM.py
@@ -2,17 +2,16 @@ import numpy as np
 import matplotlib.pyplot as plt
 import line_fitting as gf
 
-filename = "/Users/twilson/Documents/Torus/Stellar_Wind/finalgrid/Tacc7500_Tsw10000_Macc9_Msw10/halpha_turb_020_10938.dat"
-filename = "/Users/twilson/Documents/Torus/Stellar_Wind/finalgrid/Tacc6500_Tsw6000_Macc9_Msw10/halpha_060_21655.dat"
-
-
-
+filename = "halpha_020_06562.dat"
 
 print("####################Gaussian####################")
-ft = gf.fitGaussian(filename=filename,verbose=True,blueOnly=True)
+ft = gf.fitGaussian(filename=filename,verbose=False)
 
 ft.calcFWHM()
 ft.calcHWZM()
+ft.calcCentre()
+ft.calcEqWidths(wavelength=656.2)
 print("FWHM",ft.FWHM[0])
 print("HWZM",ft.HWZM[0])
+print("centre",ft.centres[0])
 print("EqWidth",ft.eqWidths)
